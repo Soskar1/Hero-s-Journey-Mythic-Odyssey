9b91b49b2302d0b9629ce1841b6dbf3b005b19b8
Soskar1 <oskar.vistorskis@gmail.com>
2023-04-10 18:48:21 +0300

    Jumping

--- Assets/_Scripts/Core/Entities/Player.cs
+++ Assets/_Scripts/Core/Entities/Player.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace HerosJourney.Core.Entities
 {
     [RequireComponent(typeof(PlayerInput))]
     [RequireComponent(typeof(ThirdPersonLook))]
+    [RequireComponent(typeof(GroundCheck))]
+    [RequireComponent(typeof(Jumping))]
     public class Player : MonoBehaviour
     {
         [SerializeField] private PlayerInput _input;
         [SerializeField] private ThirdPersonLook _thirdPerson;
+        [SerializeField] private GroundCheck _groundCheck;
+        [SerializeField] private Jumping _jumping;
         private IMovement _movement;
         private Camera _camera;
 
         private Vector2 _movementInput;
         private Vector3 _targetDirection;
-        private float _targetRotation;
 
         private void Awake()
         {
@@ -25,15 +29,23 @@ namespace HerosJourney.Core.Entities
         {
             _input.Initialize();
             _input.Enable();
+
+            _input.Controls.Player.Jump.performed += Jump;
+        }
+
+        private void OnDisable()
+        {
+            _input.Controls.Player.Jump.performed -= Jump;
+            _input.Disable();
         }
 
         private void Update()
         {
             _movementInput = _input.GetMovementDirection();
-            _targetRotation = Mathf.Atan2(_movementInput.x, _movementInput.y) * Mathf.Rad2Deg + _camera.transform.eulerAngles.y;
-            _thirdPerson.Rotate(_targetRotation);
+            float targetRotation = Mathf.Atan2(_movementInput.x, _movementInput.y) * Mathf.Rad2Deg + _camera.transform.eulerAngles.y;
+            _thirdPerson.Rotate(targetRotation);
 
-            _targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+            _targetDirection = Quaternion.Euler(0.0f, targetRotation, 0.0f) * Vector3.forward;
         }
 
         private void FixedUpdate()
@@ -42,6 +54,13 @@ namespace HerosJourney.Core.Entities
                 _movement.Move(_targetDirection);
             else 
                 _movement.Move(Vector3.zero);
+        }
+
+        private void Jump(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                if (_groundCheck.CheckForGround())
+                    _jumping.Jump();
         }
     }
 }
