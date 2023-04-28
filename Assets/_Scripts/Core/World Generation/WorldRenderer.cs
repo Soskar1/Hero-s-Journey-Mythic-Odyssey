45eefc7b6893e063dc8b412b88336a08d6bec344
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-04-28 18:43:44 +0300

    Chunk object pooling

--- Assets/_Scripts/Core/World Generation/WorldRenderer.cs
+++ Assets/_Scripts/Core/World Generation/WorldRenderer.cs
@@ -1,9 +1,43 @@
 using UnityEngine;
+using HerosJourney.Core.WorldGeneration.Chunks;
+using System.Collections.Generic;
 
 namespace HerosJourney.Core.WorldGeneration
 {
     public class WorldRenderer : MonoBehaviour
     {
+        [SerializeField] private ChunkRenderer _chunkPrefab;
+        [SerializeField] private int _initialPoolCapacity;
+        private Queue<ChunkRenderer> _chunkPool;
 
+        private void Awake() => _chunkPool = new Queue<ChunkRenderer>(_initialPoolCapacity);
+
+        public ChunkRenderer RenderChunk(ChunkData chunkData, MeshData meshData)
+        {
+            ChunkRenderer chunkRenderer;
+
+            if (_chunkPool.Count > 0)
+            {
+                chunkRenderer = _chunkPool.Dequeue();
+                chunkRenderer.gameObject.SetActive(true);
+                chunkRenderer.transform.position = chunkData.WorldPosition;
+            }
+            else
+            {
+                chunkRenderer = Instantiate(_chunkPrefab, chunkData.WorldPosition, Quaternion.identity);
+                chunkRenderer.transform.parent = transform;
+            }
+
+            chunkRenderer.InitializeChunk(chunkData);
+            chunkRenderer.UpdateChunk(meshData);
+
+            return chunkRenderer;
+        }
+
+        public void UnloadChunk(ChunkRenderer chunkRenderer)
+        {
+            _chunkPool.Enqueue(chunkRenderer);
+            chunkRenderer.gameObject.SetActive(false);
+        }
     }
 }
