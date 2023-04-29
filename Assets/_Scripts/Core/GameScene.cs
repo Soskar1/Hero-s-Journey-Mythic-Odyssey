a84d1f5b4cf1bf7fa227c474555dacdfc01f9f87
Soskar1 <oskar.vistorskis@gmail.com>
2023-04-29 10:36:37 +0300

    Multithreading bug fix, some refactoring

--- Assets/_Scripts/Core/GameScene.cs
+++ Assets/_Scripts/Core/GameScene.cs
@@ -8,12 +8,12 @@ namespace HerosJourney.Core
         [SerializeField] private GameObject _player;
         [SerializeField] private World _world;
 
-        private void OnEnable() => _world.OnNewChunksGenerated += SpawnPlayer;   
+        private void OnEnable() => _world.OnNewChunksInitialized += SpawnPlayer;   
 
         private void SpawnPlayer()
         {
             _player.SetActive(true);
-            _world.OnNewChunksGenerated -= SpawnPlayer;
+            _world.OnNewChunksInitialized -= SpawnPlayer;
         } 
     }
 }
