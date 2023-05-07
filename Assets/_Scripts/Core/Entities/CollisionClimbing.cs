45ea3512c236209eb65c09b64834022ef2ac4ba5
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-05-07 16:04:24 +0300

    camera changes + more raycasts for CollisionClimbing

--- Assets/_Scripts/Core/Entities/CollisionClimbing.cs
+++ Assets/_Scripts/Core/Entities/CollisionClimbing.cs
@@ -12,8 +12,7 @@ namespace HerosJourney.Core.Entities
         [SerializeField] private LayerMask _collisionLayerMask;
         [SerializeField] private float _stepOffset = 0.01f;
         [SerializeField] private float _maxStepHeight;
-        [SerializeField] private float _lowerRaycastMaxLength;
-        [SerializeField] private float _upperRaycastMaxLength;
+        [SerializeField] private float _raycastMaxLength;
 
         [SerializeField] private List<float> _angles = new List<float>();
 
@@ -38,14 +37,14 @@ namespace HerosJourney.Core.Entities
             heightDifference = 0;
             Quaternion rotation = Quaternion.Euler(0, rotY, 0);
 
-            if (!Physics.Raycast(_lowerStepRay.transform.position, rotation * transform.forward, _lowerRaycastMaxLength, _collisionLayerMask))
+            if (!Physics.Raycast(_lowerStepRay.transform.position, rotation * transform.forward, _raycastMaxLength, _collisionLayerMask))
                 return false;
 
-            if (Physics.Raycast(_upperStepRay.transform.position, rotation * transform.forward, _upperRaycastMaxLength, _collisionLayerMask))
+            if (Physics.Raycast(_upperStepRay.transform.position, rotation * transform.forward, _raycastMaxLength, _collisionLayerMask))
                 return false;
 
             RaycastHit hitInfo;
-            Vector3 origin = _upperStepRay.transform.position + rotation * transform.forward * _upperRaycastMaxLength;
+            Vector3 origin = _upperStepRay.transform.position + rotation * transform.forward * _raycastMaxLength;
 
             if (!Physics.Raycast(new Ray(origin, Vector3.down), out hitInfo, _maxStepHeight, _collisionLayerMask))
                 return false;
@@ -60,14 +59,14 @@ namespace HerosJourney.Core.Entities
             Gizmos.color = Color.red;
 
             foreach (float angle in _angles)
-                Gizmos.DrawLine(_lowerStepRay.position, _lowerStepRay.position + Quaternion.Euler(0, angle, 0) * transform.forward * _lowerRaycastMaxLength);
+                Gizmos.DrawLine(_lowerStepRay.position, _lowerStepRay.position + Quaternion.Euler(0, angle, 0) * transform.forward * _raycastMaxLength);
 
             Vector3 upperStepRayPosition = new Vector3(_upperStepRay.position.x, _lowerStepRay.position.y + _maxStepHeight, _upperStepRay.position.z);
 
             Gizmos.color = Color.green;
 
             foreach (float angle in _angles)
-                Gizmos.DrawLine(upperStepRayPosition, upperStepRayPosition + Quaternion.Euler(0, angle, 0) * transform.forward * _lowerRaycastMaxLength);
+                Gizmos.DrawLine(upperStepRayPosition, upperStepRayPosition + Quaternion.Euler(0, angle, 0) * transform.forward * _raycastMaxLength);
         }
     }
 }
