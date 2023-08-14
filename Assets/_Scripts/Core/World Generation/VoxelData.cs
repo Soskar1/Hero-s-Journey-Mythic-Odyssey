31ed5db1d3b62ebf6fddab572514bcc3b3023a85
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-08-14 13:52:17 +0300

    ThreadSafeVoxelData

---
+++ Assets/_Scripts/Core/World Generation/VoxelData.cs
@@ -0,0 +1,39 @@
+using System;
+using HerosJourney.Utils;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace HerosJourney.Core.WorldGeneration
+{
+    public class VoxelData : ScriptableObject
+    {
+        public int id;
+        public VoxelType type;
+        public TextureData textureData;
+        public bool generatesCollider;
+    }
+
+    public struct TSVoxelData
+    {
+        public int id;
+        public VoxelType type;
+        public TextureData textureData;
+        public bool generatesCollider;
+
+        public TSVoxelData(VoxelData voxelData)
+        {
+            id = voxelData.id;
+            type = voxelData.type;
+            textureData = voxelData.textureData;
+            generatesCollider = voxelData.generatesCollider;
+        }
+    }
+
+    [Serializable]
+    public struct TextureData
+    {
+        public int2 up;
+        public int2 down;
+        public int2 side;
+    }
+}
